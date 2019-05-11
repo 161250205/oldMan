3d16bc818d7d28cdc3569ecbade6eec845a6e5f3
161250214 <89749480@qq.com>
2019-05-11 14:52:52 +0800

    manager update

--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -10,22 +10,41 @@ public class Manager extends Worker {
 	}
 	//Manager类的初始化
 	public Manager(String name, int age, int salary, String department) {
-
+		super(name,age,salary,department);
+		this.worker = new ArrayList<Worker>();
 	}
 
 	// 管理人员可以查询本部门员工的基本信息，跨部门查询提示权限不足，提示“Access Denied!”
 	public String inquire(Worker e) {
-		return null;
+		if(e.getClass().toString().substring(6).equals(this.department)){
+			return e.show();
+		}
+		else{
+			throw new IllegalArgumentException("Access denied!");
+		}
 	}
 
 	// 管理人员给自己的队伍添加工作人员，同一部门的工作人员可以添加，并返回true，不同部门的工作人员无法添加，返回false
 	public boolean lead(Worker e) {
-		return false;
+		if(e.getClass().toString().substring(6).equals(this.department)){
+			return worker.add(e);
+		}
+		else{
+			throw new IllegalArgumentException("Access denied!");
+		}
 	}
 
 	// 打印自己队伍的人员姓名，没有打印“Empty”
 	public String print() {
-		return null;
+		String res = "Statement for a";
+		for (Worker w : this.worker){
+			StringBuilder stringBuilder = new StringBuilder();
+			stringBuilder.append(res);
+			stringBuilder.append("\n - ").append(w.name);
+			res = stringBuilder.toString();
+		}
+
+		return this.worker.isEmpty() ? "Empty" : res;
 	}
 
 }
