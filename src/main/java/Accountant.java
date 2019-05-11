b416e6cb85bb1fcd36fe9ca16ee79d7da9713eae
yujiezhang <845075906@qq.com>
2019-05-11 15:32:28 +0800

    Accountant

--- src/main/java/Accountant.java
+++ src/main/java/Accountant.java
@@ -1,15 +1,18 @@
 
 public class Accountant extends Worker {
 	public String password;
-
+	private String[] units = {"Billion","Million","Thousand","Hundred"};
+	private String[] belowtens = {"One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
+	private String[] tens = {"Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+	private String[] overtens = {"Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
 	public Accountant() {
 
 	}
 	
 	//初始化Accountant
 	public Accountant(String name, int age, int salary, String password) {
-			super(name,age,salary,"");
-
+			super(name,age,salary,"Accountant");
+			this.password = password;
 	}
 	
     /**
@@ -32,10 +35,74 @@ public class Accountant extends Worker {
      * @param number
      */
     public  String numberToWords (String number){
-		return password;
+    	int num = -1;
+    	try {
+			num = Integer.parseInt(number);
+			if (num<0)return "illegal";
+		}catch (Exception e){
+    		return "illegal";
+		}
+		if(num==-1)return "illegal";
+		return numberToWords(num);
 
     }
-    
+	private String numberToWords(int num) {
+		if(num==0)return "Zero";
+		String numStr = Integer.toString(num);
+		String temp;
+		StringBuilder res = new StringBuilder();
+		if(numStr.length()>9){
+			temp = numStr.substring(0,1);
+			res.append(belowtens[temp.charAt(0)-'1']+" Billion ");
+			numStr = numStr.substring(1);
+		}
+		if(numStr.length()>6){
+			int f = numStr.length()-6;
+			temp = numStr.substring(0,f);
+			numStr = numStr.substring(f);
+			temp = smallNumToWords(temp);
+			if(temp.length()>0)
+				res.append(temp+"Million ");
+		}
+		if(numStr.length()>3){
+			int f = numStr.length()-3;
+			temp = numStr.substring(0,f);
+			numStr = numStr.substring(f);
+			temp = smallNumToWords(temp);
+			if(temp.length()>0)
+				res.append(temp+"Thousand ");
+		}
+		res.append(smallNumToWords(numStr));
+		return res.toString().trim();
+	}
+	private String smallNumToWords(String temp){
+		if(temp==null)return null;
+		int length = temp.length();
+		int x = temp.charAt(0)-'1';
+		if(length==3){
+			if(x!=-1){
+				return belowtens[x]+" Hundred "+smallNumToWords(temp.substring(1));
+			}else{
+				return smallNumToWords(temp.substring(1));
+			}
+		}else if(length==2){
+			if(x!=-1){
+				if(x==0){
+					String t = temp.substring(1);
+					int x2 = t.charAt(0)-'0';
+					return tens[x2]+" ";
+				}
+				return overtens[x-1]+" "+smallNumToWords(temp.substring(1));
+			}else{
+				return smallNumToWords(temp.substring(1));
+			}
+		}else if(length==1){
+			if(x!=-1){
+				return belowtens[x]+" ";
+			}else return "";
+		}
+		return null;
+	}
     /**
      * 检验密码
      * 由于会计身份的特殊性，对会计的密码安全有较高的要求，
@@ -53,11 +120,50 @@ public class Accountant extends Worker {
      *
      * password: HelloWorld
      * return: 1
-     * 
+     *
      * @param password
      */
     public  int checkPassword(){
-		return 0;
+		int size = password.length();
+		int[] rt = new int[size];
+		int LowerCase = 1,UpperCase = 1,Num = 1;
+		for (int i = 0; i < size;) {
+			char c = password.charAt(i);
+			if (c >= 'a' && c <= 'z') LowerCase = 0;
+			if (c >= 'A' && c <= 'Z') UpperCase = 0;
+			if (c >= '1' && c <= '9') Num = 0;
+
+			int j = i;
+			while (i < size && c == password.charAt(i)) i++;
+			rt[j] = i - j;
+		}
+
+		int miss = LowerCase + UpperCase + Num;
+		int res = 0;
+		if (size < 8){
+			res = Math.max(miss,8 - size);
+		}else{
+			int over = Math.max(size - 20, 0), left = 0;
+			res += over;
+			for (int k = 1; k < 3; ++k) {
+				for (int i = 0; i < size && over > 0; ++i) {
+					if (rt[i] < 3 || rt[i] % 3 != (k - 1)) continue;
+					rt[i] -= Math.min(over,k);
+					over -=k;
+				}
+			}
+			for (int i = 0; i < size; ++i) {
+				if (rt[i] >= 3 && over > 0) {
+					int need = rt[i] - 2;
+					rt[i] -= over;
+					over -= need;
+				}
+				if (rt[i] >= 3) left += rt[i] / 3;
+			}
+			res += Math.max(miss, left);
+		}
+
+		return res;
 
     }
 }
